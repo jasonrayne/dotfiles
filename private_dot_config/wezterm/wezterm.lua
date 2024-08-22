313b1b4a39f3878b02a0094f06b3f0d003e22dcd
Jason Rayne <jason.rayne@biamp.com>
2024-08-22 16:03:54 -0700

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -9,12 +9,24 @@ local wayland_gnome = require 'wayland_gnome'
 wayland_gnome.apply_to_config(config)
 
 -- Show which key table is active in the status area
-wezterm.on("update-right-status", function(window, pane)
-	local name = window:active_key_table()
-	if name then
-		name = "TABLE: " .. name
-	end
-	window:set_right_status(name or "")
+wezterm.on("update-right-status", function(window)
+  local key_table = window:active_key_table()
+  local workspace = window:active_workspace()
+
+  local status = ""
+
+  if workspace then
+    status = "WORKSPACE: " .. workspace
+  end
+
+  if key_table then
+    if #status > 0 then
+      status = status .. " | "
+    end
+    status = status .. "TABLE: " .. key_table
+  end
+
+  window:set_right_status(status)
 end)
 
 -- Apply configuration
