bf40950103e138cf3800917c2a3c7bcfb2cc7084
Jason Rayne <jason.rayne@biamp.com>
2024-09-10 17:01:34 -0700

    Update .config/nvim/lua/config/options.lua

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.api.nvim_create_augroup("AnsibleYAML", { clear = true })
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  pattern = "*/kayobe-config/*",
+  command = "set filetype=yaml.ansible",
+  group = "AnsibleYAML",
+})
