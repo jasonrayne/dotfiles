1b387f4f7b4566f88226b529bac108c90ebae057
Jason Rayne <jason.rayne@biamp.com>
2025-09-08 13:35:21 -0700

    Update .config/bash/work.sh

--- private_dot_config/bash/work.sh
+++ private_dot_config/bash/work.sh
@@ -1,14 +1,20 @@
 # Only load on work machine
 if [[ $(hostname) == BSI-US-* ]]; then
+  source /usr/share/doc/fzf/examples/key-bindings.bash
   source <(openstack complete 2>/dev/null)
 
   export PATH=$PATH:/usr/local/go/bin
   export TMUX_CONFIG_DIR="$HOME/.config/tmux"
   export OS_CLOUD=biamp-cloud
+  export NVM_DIR="$HOME/.config/nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
   eval "$(zoxide init bash)"
   eval "$(starship init bash)"
   . "$HOME/.cargo/env"
 
   alias claude="/home/jrayne/.claude/local/claude"
+  alias pynvim="/home/jrayne/.local/bin/pynvim-python"
 fi
