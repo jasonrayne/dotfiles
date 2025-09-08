7ab90a2be40655e34719dd832b3e513e1efff489
Jason Rayne <jason.rayne@biamp.com>
2025-09-08 12:19:38 -0700

    Update .config/bash/work.sh

--- private_dot_config/bash/work.sh
+++ private_dot_config/bash/work.sh
@@ -1,5 +1,7 @@
 # Only load on work machine
 if [[ $(hostname) == BSI-US-* ]]; then
+  source <(openstack complete 2>/dev/null)
+
   export PATH=$PATH:/usr/local/go/bin
   export TMUX_CONFIG_DIR="$HOME/.config/tmux"
   export OS_CLOUD=biamp-cloud
